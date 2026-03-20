303116a6c67865526374c2bc71d746dd39d0b267
ankush_choudhari76 <168920858+Ankush-choudhary76@users.noreply.github.com>
2026-03-20 20:48:54 +0530

    Add files via upload

---
+++ RAG/rag-using-langchain.py
@@ -0,0 +1,20 @@
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+
+video_id = "Gfr50f6ZBvo" # only the ID, not full URL
+try:
+    # If you don’t care which language, this returns the “best” one
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["en"])
+
+    # Flatten it to plain text
+    transcript = " ".join(chunk["text"] for chunk in transcript_list)
+    print(transcript)
+
+except TranscriptsDisabled:
+    print("No captions available for this video.")
